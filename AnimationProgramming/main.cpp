c96d4254293deab8107a10155034a2978a2b0e34
Adam HAMOUCH <a.hamouch@student.isartdigital.com>
2025-02-05 11:06:17 +0100

    draw skelet (non functionnal)

--- AnimationProgramming/main.cpp
+++ AnimationProgramming/main.cpp
@@ -1,42 +1,54 @@
-// main.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
+#include <vector>
+#include <iostream>
 #include "Engine.h"
 #include "Simulation.h"
+#include "math.h"
 
 class CSimulation : public ISimulation
 {
+	std::vector<Vec3> positionsBones;
+
 	virtual void Init() override
 	{
-		int spine01 =	GetSkeletonBoneIndex("spine_01");
-		int spineParent = GetSkeletonBoneParentIndex(spine01);
-		const char* spineParentName = GetSkeletonBoneName(spineParent);
-
-		float posX, posY, posZ, quatW, quatX, quatY, quatZ;
+		size_t size = GetSkeletonBoneCount();
 		size_t keyCount = GetAnimKeyCount("ThirdPersonWalk.anim");
-		GetAnimLocalBoneTransform("ThirdPersonWalk.anim", spineParent, keyCount / 2, posX, posY, posZ, quatW, quatX, quatY, quatZ);
-		
-		printf("Spine parent bone : %s\n", spineParentName);
-		printf("Anim key count : %ld\n", keyCount);
-		printf("Anim key : pos(%.2f,%.2f,%.2f) rotation quat(%.10f,%.10f,%.10f,%.10f)\n", posX, posY, posZ, quatW, quatX, quatY, quatZ);
+
+		for (int i = 0; i < GetSkeletonBoneCount(); ++i)
+		{
+			float posX, posY, posZ, quatW, quatX, quatY, quatZ;
+			GetSkeletonBoneLocalBindTransform(i, posX, posY, posZ, quatW, quatX, quatY, quatZ);
+			//Vec3 v = Matrix4x4::Identity4x4() * InitVector(posX, posY, posZ);
+			positionsBones.push_back(Vec3(posX, posY, posZ));
+
+			if (i > 0)
+			{
+				int parent = GetSkeletonBoneParentIndex(i);
+				printf(GetSkeletonBoneName(parent));
+				printf("\n");
+			}
+		}
 	}
 
 	virtual void Update(float frameTime) override
 	{
-		// X axis
-		//DrawLine(0, 0, 0, 100, 0, 0, 1, 0, 0);
-
-		// Y axis
-		//DrawLine(0, 0, 0, 0, 100, 0, 0, 1, 0);
-
-		// Z axis
-	//	DrawLine(0, 0, 0, 0, 0, 100, 0, 0, 1);
-
-
-		DrawLine(20, -50, 0, 0, 0, 110, 0, 0, 1);
-
+		for (int i = 0; i < positionsBones.size(); ++i)
+		{
+			int p = GetSkeletonBoneParentIndex(i);
+			if (i != positionsBones.size() - 1 && p != -1)
+			{
+				DrawLine(positionsBones[i].x, positionsBones[i].y, positionsBones[i].z, positionsBones[p].x, positionsBones[p].y, positionsBones[p].z, 0, 0, 1);
+			}
+			//DrawLine(positionsBones[1].x, positionsBones[1].y, positionsBones[1].z, positionsBones[2].x, positionsBones[2].y, positionsBones[2].z, 0, 0, 1);
+			//DrawLine(positionsBones[2].x, positionsBones[2].y, positionsBones[2].z, positionsBones[3].x, positionsBones[3].y, positionsBones[3].z, 0, 0, 1);
+			//DrawLine(positionsBones[3].x, positionsBones[3].y, positionsBones[3].z, positionsBones[4].x, positionsBones[4].y, positionsBones[4].z, 0, 0, 1);
+			//DrawLine(positionsBones[4].x, positionsBones[4].y, positionsBones[4].z, positionsBones[5].x, positionsBones[5].y, positionsBones[5].z, 0, 0, 1);
+			//DrawLine(positionsBones[5].x, positionsBones[5].y, positionsBones[5].z, positionsBones[6].x, positionsBones[6].y, positionsBones[6].z, 0, 0, 1);
+			//DrawLine(positionsBones[6].x, positionsBones[6].y, positionsBones[6].z, positionsBones[7].x, positionsBones[7].y, positionsBones[7].z, 0, 0, 1);
+			//DrawLine(positionsBones[7].x, positionsBones[7].y, positionsBones[7].z, positionsBones[8].x, positionsBones[8].y, positionsBones[8].z, 0, 0, 1);
+			//DrawLine(positionsBones[8].x, positionsBones[8].y, positionsBones[8].z, positionsBones[9].x, positionsBones[9].y, positionsBones[9].z, 0, 0, 1);
+			//DrawLine(positionsBones[9].x, positionsBones[9].y, positionsBones[9].z, positionsBones[10].x, positionsBones[10].y, positionsBones[10].z, 0, 0, 1);
+		}
 	}
 };
 
@@ -45,6 +57,6 @@ int main()
 	CSimulation simulation;
 	Run(&simulation, 1400, 800);
 
-    return 0;
+	return 0;
 }
 
